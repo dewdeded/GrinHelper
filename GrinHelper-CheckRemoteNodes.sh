28dcecb9bae77f3fcb51b36d5f7ca434c96114f5
dewdeded <admin@xmr.biz>
2018-02-03 01:06:08 +0100

    Update 2018-02-03

--- GrinHelper-CheckRemoteNodes.sh
+++ GrinHelper-CheckRemoteNodes.sh
@@ -4,7 +4,7 @@
 # Needs GrinHelper ( https://github.com/dewdeded/GrinHelper on remote node)
 
 #### Configuration ####
-ConfigFile="`pwd`/GrinHelper-NodeList.conf"
+ConfigFile="$(pwd)/GrinHelper-NodeList.conf"
 UpdateURL="https://raw.githubusercontent.com/dewdeded/GrinHelper/master/GrinHelper.sh"
 BaseDir="/root/mw"
 RustDir="~/.cargo/bin"
@@ -12,115 +12,106 @@ RustDir="~/.cargo/bin"
 #### Begin main script ####
 source "$ConfigFile"
 
-
 # Function Check Stats
-option_1()
-{
-clear  
-echo -e "\033[0;33mNetwork height:			$(curl -s https://grintest.net/v1/chain | jq .height)\033[0m" 
-echo -e "\033[0;33mNetwork difficulty:		$(curl -s https://grintest.net/v1/chain | jq .total_difficulty)\033[0m"
+option_1() {
+	clear
+	echo -e "\033[0;33mNetwork height:			$(curl -s https://grintest.net/v1/chain | jq .height)\033[0m"
+	echo -e "\033[0;33mNetwork difficulty:		$(curl -s https://grintest.net/v1/chain | jq .total_difficulty)\033[0m"
 
-for host in "${hosts[@]}"; do
-    IFS=":" names=( $host )
-    echo -e "\033[0;34m\nHostname: ${names[2]} (IP: ${names[1]})\n\033[0m\n"
-    
-    ssh ${names[1]} /bin/grinhelper remote_stats
+	for host in "${hosts[@]}"; do
+		IFS=":" names=($host)
+		echo -e "\033[0;34m\nHostname: ${names[2]} (IP: ${names[1]})\n\033[0m\n"
 
-done
-echo ""
-echo -e "\033[0;33m\nPress ENTER To Return\033[0m"
-read continue
+		ssh ${names[1]} /bin/grinhelper remote_stats
+
+	done
+	echo ""
+	echo -e "\033[0;33m\nPress ENTER To Return\033[0m"
+	read continue
 }
 
 # Function Check Outputs
-option_2()
-{
-clear  
+option_2() {
+	clear
 
-for host in "${hosts[@]}"; do
-    IFS=":" names=( $host )
-    echo -e "\nHostname: ${names[2]} (IP: ${names[1]})\n"
-    cmd="export PATH=\"$BaseDir/grin/target/debug:$RustDir/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\" ; cd $BaseDir/grin/node1; grin wallet -p password outputs"
-    ssh -o LogLevel=QUIET ${names[1]} -t "$cmd"
-done
+	for host in "${hosts[@]}"; do
+		IFS=":" names=($host)
+		echo -e "\nHostname: ${names[2]} (IP: ${names[1]})\n"
+		cmd="export PATH=\"$BaseDir/grin/target/debug:$RustDir/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\" ; cd $BaseDir/grin/node1; grin wallet -p password outputs"
+		ssh -o LogLevel=QUIET ${names[1]} -t "$cmd"
+	done
 
-echo -e "\033[0;33m\nPress ENTER To Return\033[0m"
-read continue
+	echo -e "\033[0;33m\nPress ENTER To Return\033[0m"
+	read continue
 }
 
 # Function Check Balances
-option_3()
-{
-clear  
+option_3() {
+    clear
 
-for host in "${hosts[@]}"; do
-    IFS=":" names=( $host )
-    echo -e "\nHostname: ${names[2]} (IP: ${names[1]})\n"
-    cmd="export PATH=\"$BaseDir/grin/target/debug:$RustDir/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"; pushd "$BaseDir/grin/node1" > /dev/null ; grin wallet -p password info|grep Spend"
-    balance=$(ssh -o LogLevel=QUIET ${names[1]} -t "$cmd" | awk '{print $4}')
-    echo "${names[2]} has $balance"
-done
+	for host in "${hosts[@]}"; do
+		IFS=":" names=($host)
+		echo -e "\nHostname: ${names[2]} (IP: ${names[1]})\n"
+		cmd="export PATH=\"$BaseDir/grin/target/debug:$RustDir/.cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"; pushd "$BaseDir/grin/node1" > /dev/null ; grin wallet -p password info|grep Spend"
+		balance=$(ssh -o LogLevel=QUIET ${names[1]} -t "$cmd" | awk '{print $4}')
+		echo "${names[2]} has $balance"
+	done
 
-echo -e "\033[0;33m\nPress ENTER To Return\033[0m"
-read continue
+	echo -e "\033[0;33m\nPress ENTER To Return\033[0m"
+	read continue
 }
 
 # Function Update Grinhelper
-option_u()
-{
-clear  
+option_u() {
+	clear
 
-for host in "${hosts[@]}"; do
-    IFS=":" names=( $host )
-    echo -e "\nUpdating Grinhelper at Hostname: ${names[2]} (IP: ${names[1]})\n"
-    ssh ${names[1]} "wget -q $UpdateURL -O /bin/grinhelper; chmod +x /bin/grinhelper"
-    echo "Finished updating Grinhelper at ${names[2]}"
-    
-done
+	for host in "${hosts[@]}"; do
+		IFS=":" names=($host)
+		echo -e "\nUpdating Grinhelper at Hostname: ${names[2]} (IP: ${names[1]})\n"
+		ssh ${names[1]} "wget -q $UpdateURL -O /bin/grinhelper; chmod +x /bin/grinhelper"
+		echo "Finished updating Grinhelper at ${names[2]}"
 
-echo -e "\033[0;33m\nPress ENTER To Return\033[0m"
-read continue
+	done
+
+	echo -e "\033[0;33m\nPress ENTER To Return\033[0m"
+	read continue
 }
-
 
 ## Check if CLI argument passed to start updating nodes
-if [ "$1" == "update" ];
-then
+if [ "$1" == "update" ]; then
 	echo Updating nodes
-    option_u
-    exit 0;
+	option_u
+	exit 0
 fi
 
-
-
-while :
-do
-    clear
+while :; do
+	clear
 	echo "=========================================================================="
-    figlet G H   Check 
-    figlet Remote Nodes
-    echo -e "All functions, will be executed on all your Grin nodes."
-    echo "1) Check Sync & Mining Stats"
-    echo "2) Check Outputs"
-    echo "3) Check Balance"
-    echo ""
-    echo "u) Update Grinhelper"
-    echo "e) Exit"
-    echo "=========================================================================="
+	figlet G H Check
+	figlet Remote Nodes
+	echo -e "All functions, will be executed on all your Grin nodes."
+	echo "1) Check Sync & Mining Stats"
+	echo "2) Check Outputs"
+	echo "3) Check Balance"
 	echo ""
-    echo "Please select an option: " 
-    read m_menu
-	
-	case "$m_menu" in
-	
-    1) option_1;;
-    2) option_2;;
-    3) option_3;;
-    u) option_u;;
-    e) exit 0;;
-    *) echo "Error, invalid input. Press ENTER to go back."; read;;
-	
-    esac
-done
-}
+	echo "u) Update Grinhelper"
+	echo "e) Exit"
+	echo "=========================================================================="
+	echo ""
+	echo "Please select an option: "
+	read m_menu
 
+	case "$m_menu" in
+
+	1) option_1 ;;
+	2) option_2 ;;
+	3) option_3 ;;
+	u) option_u ;;
+	e) exit 0 ;;
+	*)
+		echo "Error, invalid input. Press ENTER to go back."
+		read
+		;;
+
+	esac
+done
--- GrinHelper.sh
+++ GrinHelper.sh
@@ -9,264 +9,249 @@ PathGrinLogFile=$HOME/mw/grin/server/grin.log
 # Max logfile size
 MaxLogSize=100M
 
-
 #############################################################
 
 #### Function definition ####
-## Installer Rust  
-rust_installer()
-{
-export TERM=xterm
-sudo apt-get update -y
-sudo apt-get install build-essential curl cmake figlet jq -y	
-	
-curl https://sh.rustup.rs -sSf | sh
-source $HOME/.cargo/env
-echo source $HOME/.cargo/env >> $HOME/.bashrc 
+## Installer Rust
+rust_installer() {
+	export TERM=xterm
+	sudo apt-get update -y
+	sudo apt-get install build-essential cmake -y
+
+	curl https://sh.rustup.rs -sSf | sh
+	source $HOME/.cargo/env
+	echo source $HOME/.cargo/env >>$HOME/.bashrc
 }
 
 ## Installer Clang
-clang_installer()
-{
-export TERM=xterm
-sudo apt-get update -y
-sudo apt-get install dialog psmisc -y
-dpkg-reconfigure locales
-sudo apt-get install clang-3.8 -y
+clang_installer() {
+	export TERM=xterm
+	sudo apt-get update -y
+	sudo apt-get install dialog psmisc -y
+	dpkg-reconfigure locales
+	sudo apt-get install clang-3.8 -y
 }
 
 ## Installer Grin
-main_installer()
-{
-while true; do
-     read -p "Do you wish to install Grin? "  yn
-    	case $yn in
-        [Yy]* ) break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    	esac
-done
-sudo apt-get install git psmisc -y
-echo export PATH=$HOME/mw/grin/target/debug:$PATH >> $HOME/.bashrc
-cd $HOME
-mkdir mw/
-cd mw
-git clone https://github.com/mimblewimble/grin.git
-cd grin
-git checkout milestone/testnet1
-source $HOME/.cargo/env
-cargo build --verbose
-mkdir node1 server
-cp grin.toml node1/ 
-cp grin.toml server/
-main_menu
-}
-
-## Starter Wallet Node
-my_wallet()
-{
-cd $HOME/mw/grin/node1
-export PATH=$HOME/mw/grin/target/debug:$PATH
-if [ -f "$HOME/mw/grin/node1/wallet.seed" ]
-then
-    echo Grin Node is now running, please keep this window open
-    grin wallet -p password -e listen
-else
-    echo Grin Node is now running, please keep this window open
-    grin wallet init
-    grin wallet -p password -e listen
-fi
-}
-
-## Starter Mining Server
-my_mining_server()
-{
-cd $HOME/mw/grin/server  
-export PATH=$HOME/mw/grin/target/debug/:$PATH 
-grin server -m run
-}
-
-## Starter Nonmining Server
-my_nonmining_server()
-{
-cd $HOME/mw/grin/server  
-export PATH=$HOME/mw/grin/target/debug/:$PATH 
-grin server run
-}
-
-## Show Spend Balances
-my_spendbalance()
-{
-cd $HOME/mw/grin/node1
-export PATH=/$HOME/mw/grin/target/debug/:$PATH
-if [ -f "$HOME/mw/grin/node1/wallet.seed" ]
-then
-    grin wallet -p password info
-    echo "Press ENTER To Return"
-    read continue
-else
-    grin wallet init
-    grin wallet -p password info
-    echo "Press ENTER To Return"
-    read continue
-fi
-}
-
-## Show Outputs
-my_outputs()
-{
-cd $HOME/mw/grin/node1
-export PATH=$HOME/mw/grin/target/debug/:$PATH
-if [ -f "$HOME/mw/grin/node1/wallet.seed" ]
-then
-    grin wallet -p password outputs
-    echo "Press ENTER To Return"
-    read continue
-else
-    grin wallet init
-    grin wallet -p password outputs
-    echo "Press ENTER To Return"
-    read continue
-fi
-}
-
-## Start the main part of the application menu if Grin and prerequisites are installed
-main_menu()
-{
-
-while :
-do
-    clear
-	figlet GrinHelper $host
-    
-	echo " "
-	echo -e "Please select an option\n"
-	echo "1)  Grin Wallet Server - Start detached"
-	echo "l1) Grin Wallet Server - Check logfiles"
-	echo " "
-	echo "2)  Grin Mining Node - Start detached"
-	echo "l2) Grin Mining Node - Check logfiles"
-	echo " "
-	echo "3)  Grin Regular Node (non-mining) - Start detached"
-	echo "l3) Grin Regular Node (non-mining) - Check logfiles"
-	echo " "
-	echo "8) View balance"
-	echo "9) Show outputs"
-	echo " "
-	echo "c) Check Grin processes"
-	echo "s) Check sync & mining stats"
-	echo "k) Killall Grin processes"
-	echo " "
-	echo "u) Update Grindhelper"
-	echo "e) Exit"
-	echo "====================================="
-	
-    read m_menu
-	
-	case "$m_menu" in
-	
-		1) option_1;;
-		l1) option_l1;;
-		2) option_2;;
-		l2) option_l2;;
-		3) option_3;;
-		l3) option_l3;;
-		4) option_4;;
-		5) option_5;;
-		6) option_6;;
-		c) option_c;;
-		s) option_s;;
-		k) option_k;;
-		8) option_8;;
-		9) option_9;;
-		u) option_u;;
-		e) exit 0;;
-    	*) echo "Error, invalid input. Press ENTER to go back."; read;;
-	esac
-done
-}
-
-option_1()
-{
-   ##export function, run a new shell starting the wallet/node listener
-   export -f my_wallet
-   screen -dm -S grinnode /bin/grinhelper my_wallet 
-}
-
-option_l1()
-{
-    tail -f $HOME/mw/grin/node1/grin.log
-	echo "Press ENTER To Return"
-    read continue
+main_installer() {
+	while true; do
+		read -p "Do you wish to install Grin? " yn
+		case $yn in
+		[Yy]*) break ;;
+		[Nn]*) exit ;;
+		*) echo "Please answer yes or no." ;;
+		esac
+	done
+	sudo apt-get install git psmisc -y
+	echo export PATH=$HOME/mw/grin/target/debug:$PATH >>$HOME/.bashrc
+	cd $HOME
+	mkdir mw/
+	cd mw
+	git clone https://github.com/mimblewimble/grin.git
+	cd grin
+	git checkout milestone/testnet1
+	source $HOME/.cargo/env
+	cargo build --verbose
+	mkdir node1 server
+	cp grin.toml node1/
+	cp grin.toml server/
 	main_menu
 }
 
+## Starter Wallet Node
+my_wallet() {
+	cd $HOME/mw/grin/node1
+	export PATH=$HOME/mw/grin/target/debug:$PATH
+	if [ -f "$HOME/mw/grin/node1/wallet.seed" ]; then
+		echo Grin Node is now running, please keep this window open
+		grin wallet -p password -e listen
+	else
+		echo Grin Node is now running, please keep this window open
+		grin wallet init
+		grin wallet -p password -e listen
+	fi
+}
 
-option_2()
-{
-    ##export function, run a new shell starting the server
+## Starter Mining Server
+my_mining_server() {
+	cd $HOME/mw/grin/server
+	export PATH=$HOME/mw/grin/target/debug/:$PATH
+	grin server -m run
+}
+
+## Starter Nonmining Server
+my_nonmining_server() {
+	cd $HOME/mw/grin/server
+	export PATH=$HOME/mw/grin/target/debug/:$PATH
+	grin server run
+}
+
+## Show Spend Balances
+my_spendbalance() {
+	cd $HOME/mw/grin/node1
+	export PATH=/$HOME/mw/grin/target/debug/:$PATH
+	if [ -f "$HOME/mw/grin/node1/wallet.seed" ]; then
+		grin wallet -p password info
+		echo "Press ENTER To Return"
+		read continue
+	else
+		grin wallet init
+		grin wallet -p password info
+		echo "Press ENTER To Return"
+		read continue
+	fi
+}
+
+## Show Outputs
+my_outputs() {
+	cd $HOME/mw/grin/node1
+	export PATH=$HOME/mw/grin/target/debug/:$PATH
+	if [ -f "$HOME/mw/grin/node1/wallet.seed" ]; then
+		grin wallet -p password outputs
+		echo "Press ENTER To Return"
+		read continue
+	else
+		grin wallet init
+		grin wallet -p password outputs
+		echo "Press ENTER To Return"
+		read continue
+	fi
+}
+
+## Term setup
+
+term_setup() {
+	if [ ! -f /etc/hostname ]; then
+	hostname -a >/etc/hostname
+	fi
+	host=$(cat /etc/hostname)
+	export TERM=xterm
+}
+
+## Main menu
+main_menu() {
+	while :; do
+		clear
+		figlet GrinHelper $host
+
+		echo " "
+		echo -e "Please select an option\n"
+		echo "1)  Grin Wallet Server - Start detached"
+		echo "l1) Grin Wallet Server - Check logfiles"
+		echo " "
+		echo "2)  Grin Mining Node - Start detached"
+		echo "l2) Grin Mining Node - Check logfiles"
+		echo " "
+		echo "3)  Grin Regular Node (non-mining) - Start detached"
+		echo "l3) Grin Regular Node (non-mining) - Check logfiles"
+		echo " "
+		echo "8) View balance"
+		echo "9) Show outputs"
+		echo " "
+		echo "c) Check Grin processes"
+		echo "s) Check sync & mining stats"
+		echo "k) Killall Grin processes"
+		echo " "
+		echo "u) Update Grindhelper"
+		echo "e) Exit"
+		echo "====================================="
+
+		read m_menu
+
+		case "$m_menu" in
+
+		1) option_1 ;;
+		l1) option_l1 ;;
+		2) option_2 ;;
+		l2) option_l2 ;;
+		3) option_3 ;;
+		l3) option_l3 ;;
+		4) option_4 ;;
+		5) option_5 ;;
+		6) option_6 ;;
+		c) option_c ;;
+		s) option_s ;;
+		k) option_k ;;
+		8) option_8 ;;
+		9) option_9 ;;
+		u) option_u ;;
+		e) exit 0 ;;
+		*)
+			echo "Error, invalid input. Press ENTER to go back."
+			read
+			;;
+		esac
+	done
+}
+
+option_1() {
+	##export function, run a new shell starting the wallet/node listener
+	export -f my_wallet
+	screen -dm -S grinnode /bin/grinhelper my_wallet
+}
+
+option_l1() {
+	tail -f $HOME/mw/grin/node1/grin.log
+	echo "Press ENTER To Return"
+	read continue
+	main_menu
+}
+
+option_2() {
+	##export function, run a new shell starting the server
 	export -f my_mining_server
 	screen -dm -S grinserver /bin/grinhelper my_mining_server
 }
 
-
-option_l2()
-{
-    tail -f $PathGrinLogFile
+option_l2() {
+	tail -f $PathGrinLogFile
 	echo "Press ENTER To Return"
-    read continue
+	read continue
 	main_menu
 }
 
-option_3()
-{
-	 ##export function, run a new shell starting the server
+option_3() {
+	##export function, run a new shell starting the server
 	export -f my_nonmining_server
 	screen -dm -S grinserver /bin/grinhelper my_nonmining_server
 }
 
-
-option_l3()
-{
+option_l3() {
 	tail -f $PathGrinLogFile
 	echo "Press ENTER To Return"
-    read continue
+	read continue
 	main_menu
 }
 
-option_4()
-{
+option_4() {
 	tail -f $PathGrinLogFile
 	echo "Press ENTER To Return"
-    read continue
+	read continue
 	main_menu
 }
 
-option_6()
-{
+option_6() {
 	echo "option 6"
 	##TODO
 }
 
-option_c()
-{
+option_c() {
 	echo " "
 	echo "Checking services ..."
 	echo " "
-	if ps aux | grep -q "[m]y_wallet" ; then echo "Wallet is running" ; else echo "Wallet is NOT running" ; fi
-	if ps aux | grep -q "[m]y_mining_server" ; then echo "Mining server is running" ; else echo "Mining server is NOT running" ; fi
-	if ps aux | grep -q "[m]y_nonmining_server" ; then echo "Non mining server is running" ; else echo "Non mining server is NOT running" ; fi
+	if ps aux | grep -q "[m]y_wallet"; then echo "Wallet is running"; else echo "Wallet is NOT running"; fi
+	if ps aux | grep -q "[m]y_mining_server"; then echo "Mining server is running"; else echo "Mining server is NOT running"; fi
+	if ps aux | grep -q "[m]y_nonmining_server"; then echo "Non mining server is running"; else echo "Non mining server is NOT running"; fi
 	echo " "
-	LogFileSize=`ls -lh $HOME/mw/grin/server/grin.log |awk '{print $5}'`
+	LogFileSize=$(ls -lh $HOME/mw/grin/server/grin.log | awk '{print $5}')
 	echo "Size Logfile: $LogFileSize"
 	echo " "
 	echo "Press ENTER To Return"
-    read continue
+	read continue
 	main_menu
 }
 
-option_s()
-{
+option_s() {
 	# tail -n 100 grin.log | grep Graphs|tail -n 1 |  cut -c21-
 	echo " "
 	echo "Network height:			$(curl -s https://grintest.net/v1/chain | jq .height)"
@@ -275,155 +260,152 @@ option_s()
 	echo "Local height:			$(curl -s http://127.0.0.1:13413/v1/chain | jq .height)"
 	echo "Local difficulty:		$(curl -s http://127.0.0.1:13413/v1/chain | jq .total_difficulty)"
 	echo " "
-	if ps aux | grep -q "[m]y_mining_server" ; then echo "Last graph time:		$(grep Graphs $PathGrinLogFile|tail -n 1 | awk ' { print $15 } ' | sed -e 's/;//g')"
-	echo "Graphs per second:		$(grep Graphs $PathGrinLogFile|tail -n 1 | awk ' { print $19 } ' )" ; 
-	else echo " "
-	echo "Mining server is NOT running" ; fi
+	if ps aux | grep -q "[m]y_mining_server"; then
+		echo "Last graph time:		$(grep Graphs $PathGrinLogFile | tail -n 1 | awk ' { print $15 } ' | sed -e 's/;//g')"
+		echo "Graphs per second:		$(grep Graphs $PathGrinLogFile | tail -n 1 | awk ' { print $19 } ')"
+	else
+		echo " "
+		echo "Mining server is NOT running"
+	fi
 
 	echo " "
-	
+
 	echo "Press ENTER To Return"
-    read continue
+	read continue
 	main_menu
 }
 
-option_k()
-{
+option_k() {
 	killall -9 grin
 	killall -9 screen
 	echo "Press ENTER To Return"
-    read continue
+	read continue
 	main_menu
 }
 
-option_8()
-{
+option_8() {
 	my_spendbalance
 }
 
-option_9()
-{
+option_9() {
 	my_outputs
 }
 
-
-option_u()
-{
+option_u() {
 	echo "Updating"
 	wget -q https://raw.githubusercontent.com/dewdeded/GrinHelper/master/GrinHelper.sh -O /bin/grinhelper
 	chmod +x /bin/grinhelper
 	clear
 	echo "Grinhelper update successful"
 	echo "Press ENTER To Return"
-    read continue
-   	/bin/grinhelper
-   	exit 0
+	read continue
+	/bin/grinhelper
+	exit 0
 }
 
 
+##### Autofix Logfile size
+autofix_logfilesize() {
+# Delete to big logfile
+find "$PathGrinLogFile" -size +$MaxLogSize -delete
 
-###########################
-
-# trap ctrl-c and call ctrl_c() for quitting log file vieewing
-trap ctrl_c INT
-
-function ctrl_c() {
-        echo "** Trapped CTRL-C"
+# Restart Grin services
+if [ ! -f $PathGrinLogFile ]; then
+	if ps aux | grep -q "[m]y_wallet"; then
+		screen -S grinnode -X quit
+		option_1
+	fi
+	if ps aux | grep -q "[m]y_mining_server"; then
+		screen -S grinserver -X quit
+		option_2
+	fi
+	if ps aux | grep -q "[m]y_nonmining_server"; then
+		screen -S grinserver -X quit
+		option_3
+	fi
+	screen -wipe
+fi
 }
 
-## Check if CLI argument passed to direct start node
-if [ "$1" == "my_wallet" ];
-then
-    my_wallet
-fi
-
-## Check if CLI argument passed to direct start server
-if [ "$1" == "my_mining_server" ];
-then
-	my_mining_server
-	exit 1;
-fi
-
-## Check if CLI argument passed to direct start server
-if [ "$1" == "my_nonmining_server" ];
-then
-	my_nonmining_server
-	exit 1;
-fi
-
-## Check if CLI argument passed to view remote stats
-if [ "$1" == "remote_stats" ];
-then
+remote_stats() {
 	echo "Local height:			$(curl -s http://127.0.0.1:13413/v1/chain | jq .height)"
 	echo "Local difficulty:		$(curl -s http://127.0.0.1:13413/v1/chain | jq .total_difficulty)"
-	echo "Graphs per second:		$(grep Graphs $PathGrinLogFile|tail -n 1| awk ' { print $19 } ' )"
-	LogFileSize=`ls -lh $HOME/mw/grin/server/grin.log |awk '{print $5}'`
+	echo "Graphs per second:		$(grep Graphs $PathGrinLogFile | tail -n 1 | awk ' { print $19 } ')"
+	LogFileSize=$(ls -lh $HOME/mw/grin/server/grin.log | awk '{print $5}')
 	echo "Size Logfile:			$LogFileSize"
-	
-	if ps aux | grep -q "[m]y_wallet" ; then echo "Wallet is running" ; else echo "Wallet is NOT running" ; fi
-	if ps aux | grep -q "[m]y_mining_server" ; then echo "Mining server is running" ; else echo "Mining server is NOT running" ; fi
-	if ps aux | grep -q "[m]y_nonmining_server" ; then echo "Non mining server is running" ; else echo "Non mining server is NOT running" ; fi
-	exit 1;
-fi
 
+	if ps aux | grep -q "[m]y_wallet"; then echo "Wallet is running"; else echo "Wallet is NOT running"; fi
+	if ps aux | grep -q "[m]y_mining_server"; then echo "Mining server is running"; else echo "Mining server is NOT running"; fi
+	if ps aux | grep -q "[m]y_nonmining_server"; then echo "Non mining server is running"; else echo "Non mining server is NOT running"; fi
+}
 
-## Check if CLI argument passed to view remote balance
-if [ "$1" == "remote_balance" ];
-then
-	cd $HOME/mw/grin/node1
-	export PATH=/$HOME/mw/grin/target/debug/:$PATH
-    grin wallet -p password info
-	exit 0;
-fi
+#############################################################
+### Begin Main Script
+#############################################################
 
+# Test and install script deps
+which sudo figlet jq screen curl >/dev/null 2>&1 || {
+	apt-get update -y
+	apt-get install -y sudo figlet jq screen curl
+}
 
 ## Check if Clang is installed
-if type -p clang-3.8 > /dev/null; 
-then
+if type -p clang-3.8 >/dev/null; then
 	:
 else
 	clang_installer
 	clear
-	echo Clang installed
+	echo "Clang installed, restart GrinHelper."
 	exit 0
 fi
 
 ## Check if Rust is installed
-if type -p rustc > /dev/null; 
-then
+if type -p rustc >/dev/null; then
 	:
 else
-    rust_installer
+	rust_installer
 	clear
-	echo Rust installed
+	echo "Rust installed, restart GrinHelper."
 	source $HOME/.cargo/env
 	exit 0
 fi
 
 ## Check if Grin is installed
-if [ -d "$HOME/mw/grin/" ];
-then
-
-## Autofix Logfile size
-find "$PathGrinLogFile" -size +$MaxLogSize -delete
-if [ ! -f $PathGrinLogFile ]; then
-    killall -9 grin
-        if ps aux | grep -q "[m]y_mining_server" ; then
-        option_1
-        option_2
-        fi
-        if ps aux | grep -q "[m]y_nom" ; then
-        option_1
-        option_3;
-        fi
+if [ -d "$HOME/mw/grin/" ]; then
+	main_installer
 fi
 
-if [ ! -f /etc/hostname ]; then
-    hostname -a > /etc/hostname
+# trap ctrl-c and call ctrl_c() for quitting log file viewing
+trap ctrl_c INT
+
+function ctrl_c() {
+	echo "** Trapped CTRL-C"
+}
+
+## Check if CLI argument passed to direct start wallet node
+if [ "$1" == "my_wallet" ]; then
+	my_wallet
 fi
-	host=`cat /etc/hostname`
-	export TERM=xterm
-    main_menu
-else
-    main_installer
+
+## Check if CLI argument passed to direct start grin mining server
+if [ "$1" == "my_mining_server" ]; then
+	my_mining_server
+	exit 1
 fi
+
+## Check if CLI argument passed to direct start grin nonming server
+if [ "$1" == "my_nonmining_server" ]; then
+	my_nonmining_server
+	exit 1
+fi
+
+## Check if CLI argument passed to view remote stats
+if [ "$1" == "remote_stats" ]; then
+	remote_stats
+	exit 1
+fi
+
+
+autofix_logfilesize
+term_setup
+main_menu
