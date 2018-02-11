7ed553c3fd6763a3030a9d7786ed17227032f34b
dewdeded <admin@xmr.biz>
2018-02-11 13:00:58 +0100

    Update 2018-02-11

---
+++ GrinHelper-AnonReceive.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export pw=`< /dev/urandom tr -dc A-Za-z0-9 | head -c20; echo`
+
+ssh-keygen -C "$pw@gr.in" -t rsa -N '' -f ~/.ssh/anonsend-$pw > /dev/null
+sleep 5
+cmd1="/bin/cat /Users/x/.ssh/anonsend-$pw.pub" 
+cmd2="/usr/local/bin/netcat anon.grinpool.co 9999"
+result=$($cmd1|$cmd2|tr -d '\0')
+
+if echo $result|grep -q 8888; then echo -e "\nSuccess, your key was uploaded."
+echo -e "Tell the sender to send to: $pw@gr.in\n"; 
+fi
---
+++ GrinHelper-AnonSend.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+http://anon.grinpool.co:8888/BPqh04thV5k22jjekPuQ.html
