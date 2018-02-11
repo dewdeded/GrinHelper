8da558af4b89beff6d2a7077bc6e081b9fb69b77
dewdeded <admin@xmr.biz>
2018-02-11 16:43:43 +0100

    Update 2018-02-11

--- GrinHelper-AnonReceive.sh
+++ GrinHelper-AnonReceive.sh
@@ -7,6 +7,6 @@ cmd1="/bin/cat /Users/x/.ssh/anonsend-$pw.pub"
 cmd2="/usr/local/bin/netcat anon.grinpool.co 9999"
 result=$($cmd1|$cmd2|tr -d '\0')
 
-if echo $result|grep -q 8888; then echo -e "\nSuccess, your key was uploaded."
+if echo $result|grep -q onion; then echo -e "\nSuccess, your key was uploaded."
 echo -e "Tell the sender to send to: $pw@gr.in\n"; 
 fi
--- GrinHelper-AnonSend.sh
+++ GrinHelper-AnonSend.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
+key="BPqh04thV5k22jjekPuQ"
 
-http://anon.grinpool.co:8888/BPqh04thV5k22jjekPuQ.html
+### Tor2Web-Proxies
+t2wproxies=(
+onion.link
+onion.to
+hiddenservice.net
+onion.casa
+)
+
+url1="https://g4eb3vctboop2v3o."
+
+gate="`echo $RANDOM % 4 | bc`"
+echo 
+url2=${t2wproxies[$gate]}
+url3="/$key"
+url4=".html"
+url=$url1$url2$url3$url4
+echo $url
+#curl 
